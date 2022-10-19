93d4999ddd1f7b01dbfbc26c5dc23c9ca5abe4bc
Dewayne VanHoozer <dvanhoozer@gmail.com>
2022-10-18 19:14:09 -0500

    tdv add the flowers catagory

--- ruby/lib/category.rb
+++ ruby/lib/category.rb
@@ -1,25 +1,18 @@
 # lib/category.rb
 
-
 module Category
   class InvalidCategory < RuntimeError; end
 
   CATEGORIES  = %i[
                   concert_ticket
                   fine_art
+                  flowers
                   gold_coin
                   normal
                 ]
 
   def validate_category(a_symbol)
-
-    debug_me{[
-      :a_symbol,
-      'CATEGORIES',
-      'CATEGORIES.first.class'
-    ]}
-
-    raise(InvalidCategory, "a_symbol") unless CATEGORIES.include?(a_symbol)
+    raise(InvalidCategory, "#{a_symbol}") unless CATEGORIES.include?(a_symbol)
 
     @category = a_symbol
   end
---
+++ ruby/spec/lib/category/flower_spec.rb
@@ -0,0 +1,12 @@
+# ./spec/lib/category/flowers_spec.rb
+
+require_relative '../../spec_helper.rb'
+
+RSpec.describe 'Category::Flowers' do
+
+  it 'should have specs'
+
+end
+
+__END__
+
